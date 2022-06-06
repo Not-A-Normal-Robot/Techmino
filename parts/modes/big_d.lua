ee35cb899139b14a572b00424b756079e8cd661f
NOT_A_ROBOT <76723917+Not-A-Normal-Robot@users.noreply.github.com>
2022-06-06 10:56:53 +0700

    [INCOMPLETE] Added Big Dekaris mode
    The spawn positions are inaccurate tho :/

---
+++ parts/modes/big_d.lua
@@ -0,0 +1,29 @@
+-- Dekaris
+return{
+    env={
+        noTele=true,
+        mindas=7,minarr=1,minsdarr=1,
+        sequence="bagES",
+        hook_drop=require'parts.eventsets.dWallGen'.hook_drop,
+        eventSet='big_d',
+        bg='bg2',bgm='push',
+    },
+    score=function(P)return{P.stat.row,P.stat.time}end,
+    scoreDisp=function(D)return D[1].." Lines   "..STRING.time(D[2])end,
+    comp=function(a,b)return a[1]>b[1]or a[1]==b[1]and a[2]<b[2]end,
+    getRank=function(P)
+        local L=P.stat.row
+        if L>=200 then
+            local T=P.stat.time
+            return
+            T<=120 and 5 or
+            T<=180 and 4 or
+            3
+        else
+            return
+            L>=150 and 2 or
+            L>=70 and 1 or
+            L>=20 and 0
+        end
+    end,
+}
